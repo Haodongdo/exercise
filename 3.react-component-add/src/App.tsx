142c1b4f8df712d72745d9288350e36c43f389bb
Haodongdo <141712720+Haodongdo@users.noreply.github.com>
2023-08-08 15:25:32 +0800

    Update App.tsx
    异步加法计算器

--- 3.react-component-add/src/App.tsx
+++ 3.react-component-add/src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './index.css';
 
-async function increaseRemote(a: number) {
-  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1e3));
-  return a + 1;
-}
+
 
 /**
  * 实现一个加法计数器功能，加法的计算是在服务端完成，这里由 increaseRemote 进行模拟，要求实现的功能有：
@@ -12,15 +9,24 @@ async function increaseRemote(a: number) {
  * 2. 由于是异步相加，自增过程中需要将 button 置为 disabled 状态，不可响应点击；
  */
 function App() {
+  const [init,setInit]=useState(0)
+  const [loading,setLoading]=useState(false)
+  
+  async function increaseRemote(a: number) {
+    setLoading(true)
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1e3));
+    setLoading(false)
+    setInit(Number(a)+init)
+}
   return (
     <div className="App">
       <header className="App-header">
         <div>加法计数器</div>
       </header>
       <section className="App-content">
-        <button>+1</button>
-        <button>+2</button>
-        <p>数值：{'计算结果'}</p>
+        <button onClick={()=>increaseRemote(1) disabled={loading}>+1</button>
+        <button onClick={()=>increaseRemote(2) disabled={loading}>+2</button>
+        <p>数值：{init}</p>
       </section>
     </div>
   );
